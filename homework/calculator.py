48d8f1d9ee92fdff76b6aba29e3441c16abd44dd
Joseph Schilz <joseph@schilz.org>
2016-04-10 11:54:03 -0700

    Initial commit.

---
+++ homework/calculator.py
@@ -0,0 +1,40 @@
+"""
+For your homework this week, you'll be creating a wsgi application of
+your own.
+
+You'll create an online calculator that can perform several operations.
+
+You'll need to support:
+
+  * Addition
+  * Subtractions
+  * Multiplication
+  * Division
+
+Your users should be able to send appropriate requests and get back
+proper responses. For example, if I open a browser to your wsgi
+application at `http://localhost:8080/multiple/3/5' then the response
+body in my browser should be `15`.
+
+Consider the following URL/Response body pairs as tests:
+
+```
+  http://localhost:8080/multiply/3/5 => 15
+  http://localhost:8080/add/23/42    => 65
+  http://localhost:8080/divide/6/0   => HTTP "400 Bad Request"
+```
+
+To submit your homework:
+
+  * Fork this repository (Session03).
+  * Edit this file to meet the homework requirements.
+  * Your script should be runnable using `$ python calculator.py`
+  * When the script is running, I should be able to view your
+    application in my browser.
+  * I should also be able to see a home page (http://localhost:8080/)
+    that explains how to perform calculations.
+  * Commit and push your changes to your fork.
+  * Submit a link to your Session03 fork repository!
+
+
+"""
